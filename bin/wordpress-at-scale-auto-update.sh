12d750c476f1bece2a808ef2e3a15cca8bec41ab
ataylorme <andrew@ataylor.me>
2017-04-20 14:27:15 -0500

    Force commit

--- bin/wordpress-at-scale-auto-update.sh
+++ bin/wordpress-at-scale-auto-update.sh
@@ -61,7 +61,7 @@ else
 
     # committing updated WordPress plugins
     echo -e "\nCommitting WordPress plugin updates on the ${MULTIDEV} multidev..."
-    terminus env:commit $SITE_UUID.$MULTIDEV --message="update WordPress plugins" --yes
+    terminus env:commit $SITE_UUID.$MULTIDEV --message="update WordPress plugins" --force
     UPDATES_APPLIED=true
 fi
 
@@ -84,7 +84,7 @@ else
 
     # committing updated WordPress themes
     echo -e "\nCommitting WordPress theme updates on the ${MULTIDEV} multidev..."
-    terminus env:commit $SITE_UUID.$MULTIDEV --message="update WordPress themes" --yes
+    terminus env:commit $SITE_UUID.$MULTIDEV --message="update WordPress themes" --force
     UPDATES_APPLIED=true
 fi
 
