7014aed4fcf3adf044a054c80a29e2e9c88a7d68
ataylorme <andrew@ataylor.me>
2017-11-14 15:44:12 -0800

    Add missing auto-update.sh

---
+++ .circleci/auto-update.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# login to Terminus
+echo -e "\nLogging into Terminus..."
+terminus auth:login --machine-token=${TERMINUS_MACHINE_TOKEN}
+
+# delete the multidev environment
+echo -e "\nDeleting the ${MULTIDEV} multidev environment..."
+terminus multidev:delete $SITE_UUID.$MULTIDEV --delete-branch --yes
+
+# recreate the multidev environment
+echo -e "\nRe-creating the ${MULTIDEV} multidev environment..."
+terminus multidev:create $SITE_UUID.live $MULTIDEV
+
+# check for upstream updates
+echo -e "\nChecking for upstream updates on the ${MULTIDEV} multidev..."
+# the output goes to stderr, not stdout
+UPSTREAM_UPDATES="$(terminus upstream:updates:list $SITE_UUID.$MULTIDEV  --format=list  2>&1)"
+
+if [[ ${UPSTREAM_UPDATES} == *"no available updates"* ]]
+then
+    # no upstream updates available
+    echo -e "\nNo upstream updates found on the ${MULTIDEV} multidev..."
+else
+    # making sure the multidev is in git mode
+    echo -e "\nSetting the ${MULTIDEV} multidev to git mode"
+    terminus connection:set $SITE_UUID.$MULTIDEV git
+
+    # apply WordPress upstream updates
+    echo -e "\nApplying upstream updates on the ${MULTIDEV} multidev..."
+    terminus upstream:updates:apply $SITE_UUID.$MULTIDEV --yes --updatedb --accept-upstream
+    UPDATES_APPLIED=true
+
+    terminus -n wp $SITE_UUID.$MULTIDEV -- core update-db
+fi
+
+# making sure the multidev is in SFTP mode
+echo -e "\nSetting the ${MULTIDEV} multidev to SFTP mode"
+terminus connection:set $SITE_UUID.$MULTIDEV sftp
+
+# Wake pantheon SSH
+terminus -n wp $SITE_UUID.$MULTIDEV -- cli version
+
+# check for WordPress plugin updates
+echo -e "\nChecking for WordPress plugin updates on the ${MULTIDEV} multidev..."
+PLUGIN_UPDATES=$(terminus -n wp $SITE_UUID.$MULTIDEV -- plugin list --update=available --format=count)
+echo $PLUGIN_UPDATES
+
+if [[ "$PLUGIN_UPDATES" == "0" ]]
+then
+    # no WordPress plugin updates found
+    echo -e "\nNo WordPress plugin updates found on the ${MULTIDEV} multidev..."
+else
+    # update WordPress plugins
+    echo -e "\nUpdating WordPress plugins on the ${MULTIDEV} multidev..."
+    terminus -n wp $SITE_UUID.$MULTIDEV -- plugin update --all
+
+    # wake the site environment before committing code
+    echo -e "\nWaking the ${MULTIDEV} multidev..."
+    terminus env:wake $SITE_UUID.$MULTIDEV
+
+    # committing updated WordPress plugins
+    echo -e "\nCommitting WordPress plugin updates on the ${MULTIDEV} multidev..."
+    terminus env:commit $SITE_UUID.$MULTIDEV --force --message="update WordPress plugins"
+    UPDATES_APPLIED=true
+fi
+
+# check for WordPress theme updates
+echo -e "\nChecking for WordPress theme updates on the ${MULTIDEV} multidev..."
+THEME_UPDATES=$(terminus -n wp $SITE_UUID.$MULTIDEV -- theme list --update=available --format=count)
+echo $THEME_UPDATES
+
+if [[ "$THEME_UPDATES" == "0" ]]
+then
+    # no WordPress theme updates found
+    echo -e "\nNo WordPress theme updates found on the ${MULTIDEV} multidev..."
+else
+    # update WordPress themes
+    echo -e "\nUpdating WordPress themes on the ${MULTIDEV} multidev..."
+    terminus -n wp $SITE_UUID.$MULTIDEV -- theme update --all
+
+    # wake the site environment before committing code
+    echo -e "\nWaking the ${MULTIDEV} multidev..."
+    terminus env:wake $SITE_UUID.$MULTIDEV
+
+    # committing updated WordPress themes
+    echo -e "\nCommitting WordPress theme updates on the ${MULTIDEV} multidev..."
+    terminus env:commit $SITE_UUID.$MULTIDEV --force --message="update WordPress themes"
+    UPDATES_APPLIED=true
+fi
+
+if [[ "${UPDATES_APPLIED}" = false ]]
+then
+    # no updates applied
+    echo -e "\nNo updates to apply..."
+    SLACK_MESSAGE="Circle CI update check #${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME}. No updates to apply, nothing deployed."
+    echo -e "\nSending a message to the ${SLACK_CHANNEL} Slack channel"
+    curl -X POST --data "payload={\"channel\": \"${SLACK_CHANNEL}\", \"username\": \"${SLACK_USERNAME}\", \"text\": \"${SLACK_MESSAGE}\"}" $SLACK_HOOK_URL
+else
+    # Run visual regression tests
+	curl --user ${CIRCLE_TOKEN}: \
+                --data build_parameters[CIRCLE_JOB]=visual_regression_test \
+                --data revision=$CIRCLE_SHA1 \
+                https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH
+fi
