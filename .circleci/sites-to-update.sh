37d903cd026240bd1b32bbefd10956d2392f28f6
ataylorme <andrew@ataylor.me>
2018-02-12 10:40:27 -0800

    Remove comment

--- .circleci/sites-to-update.sh
+++ .circleci/sites-to-update.sh
@@ -19,7 +19,6 @@ while IFS= read -r SITE_UUID &&
 	fi
 
 	curl --user ${CIRCLE_TOKEN}: \
-				#--data build_parameters[CIRCLE_JOB]=check_for_updates \
 				--data build_parameters[CIRCLE_JOB]=behat_test \
 				--data build_parameters[SITE_NAME]=$SITE_NAME \
 				--data build_parameters[SITE_UUID]=$SITE_UUID \
