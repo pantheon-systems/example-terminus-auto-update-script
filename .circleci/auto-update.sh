f8e4c41904cc091c8acf685b54a9e366311d3485
ataylorme <andrew@ataylor.me>
2018-05-01 08:27:01 -0700

    Correct if multidev exists function return code

--- .circleci/auto-update.sh
+++ .circleci/auto-update.sh
@@ -11,7 +11,7 @@ TERMINUS_DOES_MULTIDEV_EXIST()
     # Return 1 if on master since dev always exists
     if [[ ${CIRCLE_BRANCH} == "master" ]]
     then
-        return 1;
+        return 0;
     fi
     
     # Stash list of Pantheon multidev environments
